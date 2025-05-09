5cf18e48869cc0922fc1e7212e5177d9e1580852
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-05-09 15:38:35 +0100

    v2

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from taxi.models import *
+from taxi.models import Driver, Car, Manufacturer
 
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
